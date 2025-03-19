911d8d84b9474f51ad96f40ee2cd2f64c5cedd86
fehervendel <fehervendel@gmail.com>
2025-03-19 14:35:18 +0100

    feature: add Hero.jsx design

--- fehervendel-portfolio/src/Components/Hero.jsx
+++ fehervendel-portfolio/src/Components/Hero.jsx
@@ -1,15 +1,22 @@
+import './Hero.css';
+/*Animations needed*/
 export default function Hero() {
     return (
-        <section className="w-screen flex justify-center bg-amber-400">
-            <div className="container flex w-screen">
-                <div className="w-1/2">
-                    2313
-                </div>
-                <div className="w-1/2">
-                    asd
-                </div>
-            </div>
+        <section className="w-screen flex justify-center bg-transparent">
+            <div className="container flex flex-col-reverse lg:flex-row w-screen px-8">
 
+                <div className="lg:w-1/2 lg:pb-56 lg:mt-44 mt-96 pt-20 lg:pt-96">
+                    <h1 className="text-stone-50 uppercase font-bold text-center md:text-start pb-8 !text-5 md:!text-7xl lg:!text-8xl text-stroke md:whitespace-nowrap">Create <br></br> your <br></br> <span className="whitespace-nowrap">&#123; <span className="colorful">dream</span> &#125;</span> website</h1>
+                    {/*<div className="bg-stone-900/30 rounded-2xl">
+                        <p className="text-stone-50 p-4">Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+                    </div>*/}
+                </div>
+
+                <div className="lg:w-1/2 flex justify-center md:justify-end text-stroke pt-30">
+                    <h1 className="text-stone-50 uppercase text-center font-bold !text-6 md:!text-8xl lg:!text-9xl lg:text-end">Vendel Fehér</h1>
+                </div>
+
+            </div>
         </section>
     )
 }
