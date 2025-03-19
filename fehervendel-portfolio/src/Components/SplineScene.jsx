5df0cccab96457372562c60179f63f7d79751820
fehervendel <fehervendel@gmail.com>
2025-03-19 14:34:42 +0100

    feature: add SplineScene

---
+++ fehervendel-portfolio/src/Components/SplineScene.jsx
@@ -0,0 +1,10 @@
+import Spline from '@splinetool/react-spline';
+import './SplineScene.css';
+
+export default function SplineScene() {
+    return (
+        <div className="spline-scene">
+            <Spline scene="https://prod.spline.design/5aDxcXfdcZg6i7aG/scene.splinecode" />
+        </div>
+    );
+}
