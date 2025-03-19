0fbe7a0c5d28fdd39fc231396349641bd49c6e21
fehervendel <fehervendel@gmail.com>
2025-03-19 11:47:31 +0100

    feature: add Hero.jsx

---
+++ fehervendel-portfolio/src/Components/Hero.jsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+    return (
+        <section className="w-screen flex justify-center bg-amber-400">
+            <div className="container flex w-screen">
+                <div className="w-1/2">
+                    2313
+                </div>
+                <div className="w-1/2">
+                    asd
+                </div>
+            </div>
+
+        </section>
+    )
+}
