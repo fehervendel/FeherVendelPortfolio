a8d123745b3a7c792e9972dd5f558fddb2e3f569
fehervendel <fehervendel@gmail.com>
2025-03-19 14:37:04 +0100

    feature: add spline and starting scheme

--- fehervendel-portfolio/src/App.jsx
+++ fehervendel-portfolio/src/App.jsx
@@ -1,13 +1,25 @@
 import './App.css'
 import Header from '../src/Components/Header.jsx';
 import Hero from '../src/Components/Hero.jsx';
+import SplineScene from '../src/Components/SplineScene.jsx';
 
 function App() {
 
   return (
     <>
-        <Header />
-        <Hero />
+        <div className='z-10 pointer-events-none'>
+            <Header />
+            <Hero />
+        </div>
+        <SplineScene />
+
+        <div className="pt-[100vh]">
+            <div className='container px-8'>
+                <div>Új komponens1</div>
+                <div>Új komponens2</div>
+            </div>
+        </div>
+
     </>
   )
 }
