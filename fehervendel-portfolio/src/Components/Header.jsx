33b4dd894c5f1101d3e0cfefd563871092ea3797
fehervendel <fehervendel@gmail.com>
2025-03-19 14:35:56 +0100

    feature: add sticky scrolling behavior

--- fehervendel-portfolio/src/Components/Header.jsx
+++ fehervendel-portfolio/src/Components/Header.jsx
@@ -1,23 +1,52 @@
+import {useEffect, useState} from "react";
+
+
 export default function Header() {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    })
+
+    let cssClasses = "flex justify-center top-0 left-0 fixed w-screen py-4 transition-all duration-300 max-w-full";
+
+    if (scrolled) {
+        cssClasses += " bg-white shadow-md backdrop-blur-lg";
+    } else {
+        cssClasses += ' bg-transparent';
+    }
+
     return (
-        <section className='container flex flex-row justify-between items-center w-full'>
-            <div>
-                <img alt="logo"/>
+        <nav className={cssClasses}>
+            <div className="container px-8">
+                <div>
+                    <img alt="logo"/>
+                </div>
+                <div>
+                    <ol className="flex justify-between items-center text-stone-50">
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ol>
+                </div>
+                <div>
+                    <ul className="flex justify-between items-center text-stone-50">
+                        <li>Linkedin</li>
+                        <li>Facebook</li>
+                        <li>Email</li>
+                    </ul>
+                </div>
             </div>
-            <div>
-                <ol className="flex justify-between items-center">
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                </ol>
-            </div>
-            <div>
-                <ul className="flex justify-between items-center">
-                    <li>Linkedin</li>
-                    <li>Facebook</li>
-                    <li>Email</li>
-                </ul>
-            </div>
-        </section>
+
+        </nav>
     )
 }
