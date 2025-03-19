28f7cb6179b5769fdb82011b660d98252004b4a5
fehervendel <fehervendel@gmail.com>
2025-03-19 11:48:11 +0100

    feature: use Herader and Hero

--- fehervendel-portfolio/src/App.jsx
+++ fehervendel-portfolio/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Header from '../src/Components/Header.jsx';
+import Hero from '../src/Components/Hero.jsx';
 
 function App() {
 
   return (
     <>
-
+        <Header />
+        <Hero />
     </>
   )
 }
