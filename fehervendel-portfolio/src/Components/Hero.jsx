65e6a76503bb4f6701e53a3c77af6c0e7051c3e6
fehervendel <fehervendel@gmail.com>
2025-03-20 12:51:13 +0100

    refactor: change responsive design

--- fehervendel-portfolio/src/Components/Hero.jsx
+++ fehervendel-portfolio/src/Components/Hero.jsx
@@ -2,14 +2,12 @@ import './Hero.css';
 /*Animations needed*/
 export default function Hero() {
     return (
-        <section id='hero' className="w-screen flex justify-center bg-transparent">
-            <div className="container flex flex-col-reverse lg:flex-row w-screen px-8">
+        <section id='hero' className="flex justify-center bg-transparent">
+            <div className="container flex flex-col-reverse lg:flex-row px-8">
 
-                <div className="lg:w-1/2 lg:pb-56 lg:mt-44 mt-96 pt-20 lg:pt-96">
+                <div className="lg:w-1/2 lg:mt-44 md:pb-20 md:mt-72 sm:mt-96 mt-72
+                 pt-36 lg:pt-96">
                     <h1 className="text-stone-50 uppercase font-bold text-center md:text-start pb-8 !text-5 md:!text-7xl lg:!text-8xl text-stroke md:whitespace-nowrap">Create <br></br> your <br></br> <span className="whitespace-nowrap">&#123; <span className="colorful">dream</span> &#125;</span> website</h1>
-                    {/*<div className="bg-stone-900/30 rounded-2xl">
-                        <p className="text-stone-50 p-4">Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
-                    </div>*/}
                 </div>
 
                 <div className="lg:w-1/2 flex justify-center md:justify-end text-stroke pt-30">
