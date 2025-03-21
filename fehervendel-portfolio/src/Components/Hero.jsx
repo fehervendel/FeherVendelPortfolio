cfeca8f52addd00bf161f0ff639419cb82989617
fehervendel <fehervendel@gmail.com>
2025-03-21 11:37:02 +0100

    bugfix: fix responsive design bug

--- fehervendel-portfolio/src/Components/Hero.jsx
+++ fehervendel-portfolio/src/Components/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
 
                 <div className="lg:w-1/2 lg:mt-44 md:pb-20 md:mt-72 sm:mt-96 mt-72
                  pt-36 lg:pt-96">
-                    <h1 className="text-stone-50 uppercase font-bold text-center md:text-start pb-8 !text-5 md:!text-7xl lg:!text-8xl text-stroke md:whitespace-nowrap">Create <br></br> your <br></br> <span className="whitespace-nowrap">&#123; <span className="colorful">dream</span> &#125;</span> website</h1>
+                    <h1 className="text-stone-50 uppercase font-bold text-center md:text-start pb-52 sm:pb-8  !text-5 md:!text-7xl lg:!text-8xl text-stroke md:whitespace-nowrap">Create <br></br> your <br></br> <span className="whitespace-nowrap">&#123; <span className="colorful">dream</span> &#125;</span> website</h1>
                 </div>
 
                 <div className="lg:w-1/2 flex justify-center md:justify-end text-stroke pt-30">
