becd920f27c0b56cf0bd90da15282334ab702236
fehervendel <fehervendel@gmail.com>
2025-03-24 16:17:53 +0100

    refactor: change AnimationWrapper usage level

--- fehervendel-portfolio/src/Components/Hero.jsx
+++ fehervendel-portfolio/src/Components/Hero.jsx
@@ -6,18 +6,20 @@ export default function Hero() {
         <section id='hero' className="flex justify-center bg-transparent">
             <div className="container flex flex-col-reverse lg:flex-row px-8">
 
-                <AnimationWrapper animationClass='slide-in-bottom' delay='0.9s'>
+
                 <div className="lg:w-1/2 lg:mt-44 md:pb-20 md:mt-72 sm:mt-96 mt-72
                  pt-36 lg:pt-96">
-                    <h1 className="text-stone-50 uppercase font-bold text-center md:text-start pb-52 sm:pb-8  !text-5 md:!text-7xl lg:!text-8xl text-stroke md:whitespace-nowrap">Create <br></br> your <br></br> <span className="whitespace-nowrap">&#123; <span className="colorful">dream</span> &#125;</span> website</h1>
+                    <AnimationWrapper animationClass='slide-in-bottom' delay='0.9s'>
+                        <h1 className="text-stone-50 uppercase font-bold text-center md:text-start pb-52 sm:pb-8  !text-5 md:!text-7xl lg:!text-8xl text-stroke md:whitespace-nowrap">Create <br></br> your <br></br> <span className="whitespace-nowrap">&#123; <span className="colorful">dream</span> &#125;</span> website</h1>
+                    </AnimationWrapper>
                 </div>
-                </AnimationWrapper>
 
-                <AnimationWrapper animationClass='slide-in-bottom' delay='0.7s'>
+
                     <div className="lg:w-1/2 flex justify-center md:justify-end text-stroke pt-30">
-                        <h1 className="text-stone-50 uppercase text-center font-bold !text-6 md:!text-8xl lg:!text-9xl lg:text-end">Vendel Fehér</h1>
+                        <AnimationWrapper animationClass='slide-in-bottom' delay='0.7s'>
+                            <h1 className="text-stone-50 uppercase text-center font-bold !text-6 md:!text-8xl lg:!text-9xl lg:text-end">Vendel Fehér</h1>
+                        </AnimationWrapper>
                     </div>
-                </AnimationWrapper>
 
 
             </div>
