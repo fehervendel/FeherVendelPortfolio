0e43796069410fee03b60869510e98705ea7cb86
fehervendel <fehervendel@gmail.com>
2025-03-25 16:59:51 +0100

    refactor: change font size

--- fehervendel-portfolio/src/Components/Intro.jsx
+++ fehervendel-portfolio/src/Components/Intro.jsx
@@ -43,13 +43,13 @@ export default function Intro({ setIsWelcome }) {
     }, []);
 
     return (
-        <div ref={comp} className="relative w-[100%] px-16">
+        <div ref={comp} className="relative w-[100%] lg:px-16">
             <div id="intro-slider" className="h-screen p-10 bg-stone-50 absolute top-0 justify-between left-0 z-10 w-full flex flex-col lg:flex-row items-center tracking-tight overflow-x-hidden">
-                <h1 id="title-1" className="!text-9xl font-bold lg:!text-8xl xl:!text-9xl text-black big-shadow uppercase text-center !h-1/3">Code</h1>
+                <h1 id="title-1" className="!text-8xl font-bold lg:!text-8xl xl:!text-9xl text-black big-shadow uppercase text-center !h-1/3">Code</h1>
                 <span id='dot-1' className="xl:pb-40 lg:pb-52 !text-9xl text text-black">&bull;</span>
-                <h1 id="title-2" className="!text-9xl font-bold lg:!text-8xl xl:!text-9xl text-black big-shadow uppercase text-center !h-1/3">Flow</h1>
+                <h1 id="title-2" className="!text-8xl font-bold lg:!text-8xl xl:!text-9xl text-black big-shadow uppercase text-center !h-1/3">Flow</h1>
                 <span id='dot-2' className="xl:pb-40 lg:pb-52 !text-9xl text text-black">&bull;</span>
-                <h1 id="title-3" className="!text-9xl font-bold lg:!text-8xl xl:!text-9xl text-black big-shadow text-nowrap text-center uppercase !h-1/3">Grow</h1>
+                <h1 id="title-3" className="!text-8xl font-bold lg:!text-8xl xl:!text-9xl text-black big-shadow text-nowrap text-center uppercase !h-1/3">Grow</h1>
             </div>
             <div className='h-screen flex justify-center place-items-center overflow-x-hidden'>
                 <h1 id="welcome" className="text-7xl sm:!text-9xl text-gray-50 uppercase font-bold neon-shadow">Welcome</h1>
