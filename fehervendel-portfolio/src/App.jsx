0cda0b5f099996f848d921f38584365b93d2e867
fehervendel <fehervendel@gmail.com>
2025-03-25 16:59:38 +0100

    feature: add card animation logic and mobile view background blur logic to App.jsx

--- fehervendel-portfolio/src/App.jsx
+++ fehervendel-portfolio/src/App.jsx
@@ -5,10 +5,17 @@ import SplineScene from '../src/Components/SplineScene.jsx';
 import { useState, useRef, useEffect } from "react";
 import Intro from "../src/Components/Intro.jsx";
 import AnimationWrapper from "./Components/AnimationWrapper.jsx";
+import Card from "../src/components/Card.jsx";
+import {useInView} from "react-intersection-observer";
 
 function App() {
     const [isWelcome, setIsWelcome] = useState(true);
     const contentRef = useRef(null);
+
+    const { ref, inView } = useInView({
+        triggerOnce: false,
+        threshold: 0.7
+    });
 
     useEffect(() => {
         if (!isWelcome && contentRef.current) {
@@ -37,41 +44,59 @@ function App() {
                 <Intro setIsWelcome={setIsWelcome} />
             ) : (
                 <div id="main-content" ref={contentRef} style={{ opacity: 0 }}>
+                    { inView && <div id="bg-blur" className="backdrop-blur-lg blur-animation"></div>}
                     <Header/>
 
                     <div className='w-full'>
                         <SplineScene/>
-                        <div className='relative z-10 pointer-events-none'>
+                        <div className='relative pointer-events-none'>
                             <Hero/>
                         </div>
                     </div>
 
                     <div className="container px-8">
-                        <AnimationWrapper animationClass='slide-in-bottom' delay='0.2s'>
-                            <div>
-                                1111111111111
+
+
+                        <div className="stack-area w-full h-[300vh] flex flex-col lg:flex-row">
+                            <div className="left lg:basis-[50%] flex flex-col lg:justify-center lg:h-screen text-stone-50 pe-16">
+                                <AnimationWrapper delay='0.3s'>
+                                    <h2 className="text-7xl uppercase font-bold pb-16">About me</h2>
+                                </AnimationWrapper>
+                                <AnimationWrapper delay='0.5s'>
+                                    <p className="text-xl pb-16">Description description description description description description description <br/>description description description</p>
+                                </AnimationWrapper>
+                                <AnimationWrapper delay='0.7s'>
+                                    <button className="!text-xl text-stone-950 hover:!bg-stone-950 hover:text-stone-50 hover:!border-stone-50 text-nowrap">Contact me</button>
+                                </AnimationWrapper>
                             </div>
-                        </AnimationWrapper>
-                        <AnimationWrapper animationClass='slide-in-bottom' delay='0.4s'>
-                        <div>
-                            2222222222222
+                            <div ref={ref} className="right lg:basis-[50%] h-screen relative z-10">
+                                <Card
+                                    color="bg-amber-400"
+                                    title='React'
+                                    shortDescription={"My favorite frontend javascript framework. This page is also designed with React."}/>
+
+                                <Card
+                                    color="bg-[#f13e78]"
+                                    title='React'
+                                    shortDescription={"My favorite frontend javascript framework. This page is also designed with React."}/>
+
+                                <Card
+                                    color="bg-[#585cfd]"
+                                    title='React'
+                                    shortDescription={"My favorite frontend javascript framework. This page is also designed with React."}/>
+
+                                <Card
+                                    color="bg-[#c2d834]"
+                                    title='React'
+                                    shortDescription={"My favorite frontend javascript framework. This page is also designed with React."}/>
+                            </div>
                         </div>
-                        </AnimationWrapper>
-                        <AnimationWrapper animationClass='slide-in-bottom' delay='0.6s'>
-                        <div>
-                            3333333333333
-                        </div>
-                        </AnimationWrapper>
-                        <div>Ide új tartalom jön</div>
-                        <div>Ide új tartalom jön</div>
-                        <div>Ide új tartalom jön</div>
-                        <div>Ide új tartalom jön</div>
-                        <div>Ide új tartalom jön</div>
-                        <div>Ide új tartalom jön</div>
-                        <div>Ide új tartalom jön</div>
-                        <div>Ide új tartalom jön</div>
+
+
+                        <div className="text-stone-50 h-[100vh] pt-8"><p>NEXT CONTENT</p></div>
                     </div>
                 </div>
+
             )}
         </>
     );
