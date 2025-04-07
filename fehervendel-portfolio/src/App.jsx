a2a63f36a861d665693806b443bbd0d4dd5ade82
fehervendel <fehervendel@gmail.com>
2025-04-07 12:45:12 +0200

    feature: add React router

--- fehervendel-portfolio/src/App.jsx
+++ fehervendel-portfolio/src/App.jsx
@@ -1,113 +1,15 @@
-import './App.css';
-import Header from '../src/Components/Header.jsx';
-import Hero from '../src/Components/Hero.jsx';
-import SplineScene from '../src/Components/SplineScene.jsx';
-import { useState, useRef, useEffect } from "react";
-import Intro from "../src/Components/Intro.jsx";
-import AnimationWrapper from "./Components/AnimationWrapper.jsx";
-import Card from "../src/components/Card.jsx";
-import {useInView} from "react-intersection-observer";
-import Form from "../src/Components/Form.jsx";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./Components/HomePage";
+import EditPage from "./Components/EditPage";
 
-function App() {
-    const [isWelcome, setIsWelcome] = useState(true);
-    const contentRef = useRef(null);
-    const [cards, setCards] = useState([]);
-
-    const { ref, inView } = useInView({
-        triggerOnce: false,
-        threshold: 0.7
-    });
-
-    const fetchCards = async () => {
-        try {
-            const response = await fetch("https://localhost:7217/Card/GetCards", {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json"
-                }
-            })
-
-            const jsonData = await response.json().then();
-            setCards(jsonData);
-        } catch (err){
-            console.error(err);
-        }
-    }
-
-    useEffect(() => {
-        fetchCards();
-    }, [])
-
-
-    useEffect(() => {
-        if (!isWelcome && contentRef.current) {
-            let opacity = 0;
-
-            const delayTimeout = setTimeout(() => {
-                const interval = setInterval(() => {
-                    opacity += 0.05;
-                    if (opacity >= 1) {
-                        opacity = 1;
-                        clearInterval(interval);
-                    }
-                    contentRef.current.style.opacity = opacity;
-                }, 30);
-
-                return () => clearInterval(interval);
-            }, 1000);
-
-            return () => clearTimeout(delayTimeout);
-        }
-    }, [isWelcome]);
-
-    return (
-        <>
-            {isWelcome ? (
-                <Intro setIsWelcome={setIsWelcome} />
-            ) : (
-                <div id="main-content" ref={contentRef} style={{ opacity: 0 }}>
-                    { inView && <div id="bg-blur" className="backdrop-blur-lg blur-animation"></div>}
-                    <Header/>
-
-                    <div className='w-full'>
-                        <SplineScene/>
-                        <div className='relative pointer-events-none'>
-                            <Hero/>
-                        </div>
-                    </div>
-
-                    <div className="container px-8">
-
-
-                        <div className={`stack-area w-full flex flex-col lg:flex-row`} style={{height: `${100 + 50 * cards.length}vh`}}>
-                            <div className="left lg:basis-[50%] flex flex-col lg:justify-center lg:h-screen text-stone-50 pe-16">
-                                <AnimationWrapper delay='0.3s'>
-                                    <h2 className="text-7xl uppercase font-bold pb-16">About me</h2>
-                                </AnimationWrapper>
-                                <AnimationWrapper delay='0.5s'>
-                                    <p className="text-xl pb-16">Description description description description description description description <br/>description description description</p>
-                                </AnimationWrapper>
-                                <AnimationWrapper delay='0.7s'>
-                                    <button className="!text-xl text-stone-950 hover:!bg-stone-950 hover:text-stone-50 hover:!border-stone-50 text-nowrap">Contact me</button>
-                                </AnimationWrapper>
-                            </div>
-                            <div ref={ref} className="right lg:basis-[50%] h-screen relative z-10">
-                                {cards && cards.sort((a, b) => a.order - b.order).map(card => (
-                                    <Card key={card.id} color={card.color} title={card.title} shortDescription={card.description} />
-                                ))}
-                            </div>
-                        </div>
-
-
-                        <div className="text-stone-50 pt-8 pb-32"><Form/></div>
-                    </div>
-                </div>
-
-            )}
-        </>
+export default function App() {
+    return(
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/edit" element={<EditPage />} />
+            </Routes>
+        </Router>
     );
 }
-
-export default App;
