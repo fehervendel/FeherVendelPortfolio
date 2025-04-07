d283429869716180b7e729383e10fcd025f07408
fehervendel <fehervendel@gmail.com>
2025-04-07 17:12:19 +0200

    feature: add EditPage, list and edit content and cards

--- fehervendel-portfolio/src/Components/EditPage.jsx
+++ fehervendel-portfolio/src/Components/EditPage.jsx
@@ -1,17 +1,47 @@
+import { useEffect, useState} from "react";
+import ListModel from "./ListModel.jsx";
+
 export default function EditPage() {
+    const [models, setModels] = useState([]);
+    const [rightContent, setRightContent] = useState("");
+
+    const fetchModels = async () => {
+        try {
+            const response = await fetch("https://localhost:7217/Model/GetModels", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }
+            })
+
+            const jsonData = await response.json().then();
+            setModels(jsonData);
+        } catch (err){
+            console.error(err);
+        }
+    }
+
+    useEffect(() => {
+        fetchModels();
+    }, [])
+
+    const handleSubmit = (e, model) => {
+            setRightContent(model);
+    }
+
     return (
-        <div className="text-stone-50 container px-8">
-            <div className="flex">
-                <aside className="flex-1/3">
+        <div className="text-stone-50 container px-8 pt-16 text-xl">
+            <div className="flex gap-16">
+                <aside className="flex-1/4 h-full bg-stone-800 rounded-2xl px-4 pt-8 pb-0">
                     <ul>
-                        <li>1</li>
-                        <li>2</li>
-                        <li>3</li>
-                        <li>4</li>
+                        {models.map((model, index) => (
+                            <li className="pb-8" key={index}><button onClick={(e) => handleSubmit(e, model.substring(17))} className="!bg-transparent">{model.substring(17) + "s"}</button></li>
+                        ))}
                     </ul>
                 </aside>
-                <div className="flex-2/3">
-                    Valami
+                <div className="flex-3/4">
+                    { rightContent === "" ? "Choose a model!" : <ListModel content={rightContent} /> }
                 </div>
             </div>
 
