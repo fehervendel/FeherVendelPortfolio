53f0d2761360af6417f477e9a75487fbd4c5b215
fehervendel <fehervendel@gmail.com>
2025-04-07 12:23:42 +0200

    feature: add Accordion.jsx

---
+++ fehervendel-portfolio/src/Components/Accordion.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function Accordion({ title, content }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div>
+            <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="w-full text-left py-4 !text-xl font-medium text-stone-100 flex justify-between items-center !bg-transparent focus:!outline-0 hover:!border-stone-50"
+            >
+                {title}
+                <span>{isOpen ? "▲" : "▼"}</span>
+            </button>
+
+            <div
+                className={`overflow-hidden transition-all duration-500 ease-in-out ${
+                    isOpen ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
+                }`}
+            >
+                <div className="pb-4 pt-2 text-stone-300 text-xl">
+                    {content}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Accordion;
