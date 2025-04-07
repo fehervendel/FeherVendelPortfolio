22401930f8e88138926a1c2f57f860d2cf5786ff
fehervendel <fehervendel@gmail.com>
2025-04-07 12:45:56 +0200

    feature: add EditPage.jsx - basic structure

---
+++ fehervendel-portfolio/src/Components/EditPage.jsx
@@ -0,0 +1,20 @@
+export default function EditPage() {
+    return (
+        <div className="text-stone-50 container px-8">
+            <div className="flex">
+                <aside className="flex-1/3">
+                    <ul>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                        <li>4</li>
+                    </ul>
+                </aside>
+                <div className="flex-2/3">
+                    Valami
+                </div>
+            </div>
+
+        </div>
+    )
+}
