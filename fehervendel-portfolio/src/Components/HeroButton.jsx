d529f2243a4724d8b05f31875a803704f0d10747
fehervendel <fehervendel@gmail.com>
2025-04-14 12:10:35 +0200

    feature: add target to HeroButton.jsx

--- fehervendel-portfolio/src/Components/HeroButton.jsx
+++ fehervendel-portfolio/src/Components/HeroButton.jsx
@@ -1,5 +1,5 @@
-export default function HeroButton({children, link, handleMenuClick}) {
+export default function HeroButton({children, link, handleMenuClick, ...props}) {
     return (
-        <li onClick={handleMenuClick} className="px-2"><a href={link} className='text-xl pointer-events-auto underline-animation !text-stone-50'>{children}</a></li>
+        <li onClick={handleMenuClick} className="px-2"><a {...props} href={link} className='text-xl pointer-events-auto underline-animation !text-stone-50'>{children}</a></li>
     )
 }
