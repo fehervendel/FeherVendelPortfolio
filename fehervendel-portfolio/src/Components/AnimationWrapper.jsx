a6832abc613c0e6d2bbde4e5f68a2d57963e9c79
fehervendel <fehervendel@gmail.com>
2025-04-16 22:06:08 +0200

    refactor: change inView threshold

--- fehervendel-portfolio/src/Components/AnimationWrapper.jsx
+++ fehervendel-portfolio/src/Components/AnimationWrapper.jsx
@@ -3,7 +3,7 @@ import { useInView } from 'react-intersection-observer';
 export default function AnimationWrapper({children, animationClass = 'slide-in-bottom', delay = '0s'}) {
     const { ref, inView } = useInView({
         triggerOnce: true,
-        threshold: 0.5
+        threshold: 0.3
     });
 
     return (
